8eda5d761ce48c862594052eb19a6ef1b0a939a2
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-05 19:06:14 +0530

    Spring Setup properly

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,5 +1,15 @@
 package org.example;
 
-public class App {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import static org.springframework.boot.autoconfigure.SpringBootApplication.*;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
