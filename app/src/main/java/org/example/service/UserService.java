0a99e4a352b4a0dfc2fca3469aaa06630b1e2d9f
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-05 19:25:19 +0530

    UserService made

--- app/src/main/java/org/example/service/UserService.java
+++ app/src/main/java/org/example/service/UserService.java
@@ -1,5 +1,34 @@
 package org.example.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class User {
+        private String name;
+        private int age;
+        private String address;
+    }
+
+    private User user;
+
+    public UserService() {
+        user = new User("Abhinav Kaushik", 19, "Ghaziabad, India");
+    }
+
+    public void logIn() {
+        System.out.println("Logging user in");
+    }
+
+    public void logout() throws Exception {
+        System.out.println("Logging user out");
+        throw new Exception("Unable to logout the user");
+    }
 }
