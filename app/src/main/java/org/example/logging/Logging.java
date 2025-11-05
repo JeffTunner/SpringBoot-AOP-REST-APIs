dc76b7a7e4c16fdd7a3af4f279b4d27a81bf5314
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-05 19:31:33 +0530

    AOP

---
+++ app/src/main/java/org/example/logging/Logging.java
@@ -0,0 +1,21 @@
+package org.example.logging;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class Logging {
+
+    @Before("execution(public void springaoptut.service.UserService.logIn())")
+    public void loggingAdvice1() {
+        System.out.println("Running Before Advice for LogIn");
+    }
+
+    @After("execution(public void springaoptut.service.UserService.logIn())")
+    public void loggingAdvice2() {
+        System.out.println("Running After Advice for Login");
+    }
+}
